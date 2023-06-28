8640569c21ffc1cf75c8c4117efab00dbc098b23
Jakub Becker <jakub.becker@banqsoft.pl>
2023-06-28 19:49:57 +0200

    Create a http client service and its interface

---
+++ API Rejestr WL/Services/HttpClientService.cs
@@ -0,0 +1,44 @@
+﻿using API_Rejestr_WL.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace API_Rejestr_WL.Services;
+
+public class HttpClientService : IHttpClientService
+{
+    private const string DateTimeFormat = "dd-MM-yyyy HH:mm:ss";
+    private const string WLApiAddress = "https://wl-api.mf.gov.pl/api/search/nip/";
+    private readonly HttpClient _httpClient;
+    public HttpClientService()
+    {
+        _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(WLApiAddress)
+        };
+        _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+    }
+
+    public async Task<CompanyModel?> FetchCompany(string nip)
+    {
+        if(string.IsNullOrWhiteSpace(nip))
+        {
+            return null;
+        }
+
+        var currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+        var httpResponse = await _httpClient.GetAsync($"{nip}?date={currentDate}").ConfigureAwait(false);
+        if (httpResponse == null || !httpResponse.IsSuccessStatusCode)
+        { 
+            return null; 
+        }    
+
+        var body = await httpResponse.Content.ReadAsStringAsync();
+        var response = JsonConvert.DeserializeObject<ApiResponse>(body);
+        if(response?.Result == null)
+        {
+            return null;
+        }
+
+        return response.Result.Subject;
+    }
+}
---
+++ API Rejestr WL/Services/IHttpClientService.cs
@@ -0,0 +1,8 @@
+﻿using API_Rejestr_WL.Models;
+
+namespace API_Rejestr_WL.Services;
+
+public interface IHttpClientService
+{
+    Task<CompanyModel?> FetchCompany(string nip);
+}
