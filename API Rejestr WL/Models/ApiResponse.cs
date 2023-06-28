6392205d1044f18536e0be6b8ee05e3c55e933f0
Jakub Becker <jakub.becker@banqsoft.pl>
2023-06-28 19:49:29 +0200

    Add api response model

---
+++ API Rejestr WL/Models/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Rejestr_WL.Models;
+
+public class ApiResponse
+{
+    public ApiResponseModel? Result { get; set; }
+}
+public class ApiResponseModel
+{
+    public CompanyModel? Subject { get; set; }
+    public string? RequestDateTime { get; set; }
+    public string? RequestId { get; set; }
+}
