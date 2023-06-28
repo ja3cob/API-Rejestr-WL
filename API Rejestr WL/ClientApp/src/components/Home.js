a62fc92ab2e1794426a65c61dc8035bea62a8ad5
Jakub Becker <jakub.becker@banqsoft.pl>
2023-06-28 23:59:53 +0200

    Add search function with fetch

--- API Rejestr WL/ClientApp/src/components/Home.js
+++ API Rejestr WL/ClientApp/src/components/Home.js
@@ -1,18 +1,33 @@
+import { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
-function Search() {
-  console.log("search");
-}
-
 export function Home() {
+  const [loader, setLoader] = useState(false);
+  const [table, setTable] = useState(false);
+  const inputRef = useRef(null);
+  const navigate = useNavigate();
+
+  async function Search() {
+    setLoader(true);
+    setTable(true);
+
+    const result = await fetch(`api/nip/${inputRef.current.value}`);
+    console.log(result.ok);
+  }
+  
     return (
         <div className="home-container">
-            <div className='search-container'>
+            <div className={table ? 'search-container moved' : 'search-container'}>
                 <p>Wyszukaj przedsiębiorcę</p>
                 <div className='search-controls'>
-                    <input className='search-input' type='text' placeholder='Wprowadź NIP...' />
-                    <button onClick={Search}><i class='fa fa-search' /></button>
+                    <input ref={inputRef} className='search-input' type='text' placeholder='Wprowadź NIP...' />
+                    <button onClick={Search}><i className='fa fa-search' /></button>
+                    <div className={loader ? 'loader' : 'loader hidden'} />
                 </div>
+            </div>
+            <div className='table-container'>
+                
             </div>
         </div>
     );
