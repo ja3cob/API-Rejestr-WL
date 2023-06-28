6f48a77e5966876349d429e7b0951ede368a1a38
Jakub Becker <jakub.becker@banqsoft.pl>
2023-06-28 19:49:17 +0200

    Add person and company models

---
+++ API Rejestr WL/Models/CompanyModel.cs
@@ -0,0 +1,25 @@
+﻿namespace API_Rejestr_WL.Models;
+
+public class CompanyModel
+{
+    public string Name { get; set; }
+    public string? Nip { get; set; }
+    public string? StatusVat { get; set; }
+    public string? Regon { get; set; }
+    public string? Pesel { get; set; }
+    public string? Krs { get; set; }
+    public string? ResidenceAddress { get; set; }
+    public string? WorkingAddress { get; set; }
+    public List<PersonModel>? Representatives { get; set; }
+    public List<PersonModel>? AuthorizedClerks { get; set; }
+    public List<PersonModel>? Partners { get; set; }
+    public DateTime? RegistrationLegalDate { get; set; }
+    public DateTime? RegistrationDenialDate { get; set; }
+    public string? RegistrationDenialBasis { get; set; }
+    public DateTime? RestorationDate { get; set; }
+    public string? RestorationBasis { get; set; }
+    public DateTime? RemovalDate { get; set; }
+    public string? RemovalBasis { get; set; }
+    public List<string>? AccountNumbers { get; set; }
+    public bool? HasVirtualAccounts { get; set; }
+}
---
+++ API Rejestr WL/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Rejestr_WL.Models
+{
+    public class PersonModel
+    {
+        public string? CompanyName { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Pesel { get; set; }
+        public string? Nip { get; set; }
+    }
+}
