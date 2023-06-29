c44f2bb51e8586f74437a8426d1a40b6dc1c1ee9
Jakub Becker <jakub.becker@banqsoft.pl>
2023-06-29 20:46:14 +0200

    Change to file-scoped namespace

--- API Rejestr WL/Models/PersonModel.cs
+++ API Rejestr WL/Models/PersonModel.cs
@@ -1,11 +1,10 @@
-﻿namespace API_Rejestr_WL.Models
+﻿namespace API_Rejestr_WL.Models;
+
+public class PersonModel
 {
-    public class PersonModel
-    {
-        public string? CompanyName { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Pesel { get; set; }
-        public string? Nip { get; set; }
-    }
+    public string? CompanyName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Pesel { get; set; }
+    public string? Nip { get; set; }
 }
--- API Rejestr WL/Program.cs
+++ API Rejestr WL/Program.cs
@@ -1,41 +1,37 @@
-
 using API_Rejestr_WL.Services;
 
-namespace API_Rejestr_WL
+namespace API_Rejestr_WL;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddSingleton<IHttpClientService, HttpClientService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddSingleton<IHttpClientService, HttpClientService>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UsePathBase(new PathString("/api"));
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-
-            app.MapFallbackToFile("index.html");
-
-            app.Run();
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UsePathBase(new PathString("/api"));
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
     }
 }
