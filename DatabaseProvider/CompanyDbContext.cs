7a9d8a3bcd4e22f3eef42b8365a23a3ce2da1489
Jakub Becker <jakub.becker@banqsoft.pl>
2023-06-29 23:07:51 +0200

    Add db context

---
+++ DatabaseProvider/CompanyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatabaseProvider.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.Sqlite;
+
+namespace DatabaseProvider;
+
+internal class CompanyDbContext : DbContext
+{
+    public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Company>()
+            .HasMany(c => c.AuthorizedClerks);
+        modelBuilder.Entity<Company>()
+            .HasMany(c => c.Partners);
+        modelBuilder.Entity<Company>()
+            .HasMany(c => c.Representatives);
+        modelBuilder.Entity<Company>()
+            .HasMany(c => c.AccountNumbers);
+
+        base.OnModelCreating(modelBuilder);
+    }
+    public DbSet<Company> Companies { get; set; }
+}
