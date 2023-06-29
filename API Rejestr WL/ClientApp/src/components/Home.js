49dd307d3f8e363d94b2f0d907a086d9d2ac2e14
Jakub Becker <jakub.becker@banqsoft.pl>
2023-06-29 18:37:34 +0200

    Add logic for fetching the data

--- API Rejestr WL/ClientApp/src/components/Home.js
+++ API Rejestr WL/ClientApp/src/components/Home.js
@@ -1,22 +1,38 @@
 import { useState, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
 import './Home.css';
 
 export function Home() {
-  const [loader, setLoader] = useState(false);
-  const [table, setTable] = useState(false);
+    const [loader, setLoader] = useState(false);
+    const [table, setTable] = useState(false);
+    const [data, setData] = useState(null);
     const [cross, setCross] = useState(false);
-  const inputRef = useRef(null);
-  const navigate = useNavigate();
+    const inputRef = useRef(null);
 
-  async function Search() {
-    setLoader(true);
-    setTable(true);
+    async function Search() {
+        setCross(false);
+        setLoader(true);
 
-    const result = await fetch(`api/nip/${inputRef.current.value}`);
-    console.log(result.ok);
-  }
-  
+        try {
+            const result = await fetch(`api/nip/${inputRef.current.value}`);
+            if(result.ok) {
+                const fetched = await result.json();
+                setData(fetched);
+                setTable(true);
+            }
+            else {
+                setData(null);
+                setCross(true);
+                setTable(false);
+            }
+        } catch(exception) {
+            setData(null);
+            setTable(false);
+            console.log(`Error fetching data: ${exception}`);
+        } finally {
+            setLoader(false);
+        }
+    }
+
     return (
         <div className="home-container">
             <div className={table ? 'search-container moved' : 'search-container'}>
@@ -29,7 +45,11 @@ export function Home() {
                 </div>
             </div>
             <div className='table-container'>
-                
+                {data ? (
+                        <span>{JSON.stringify(data, null, 2)}</span>
+                ) : (
+                    <p>No data to display.</p>
+                )}
             </div>
         </div>
     );
