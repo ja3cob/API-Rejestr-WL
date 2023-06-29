651b1b272d913aa98ecc81fd8fea59b1f6b683bc
Jakub Becker <jakub.becker@banqsoft.pl>
2023-06-29 23:05:37 +0200

    Add more entities

---
+++ DatabaseProvider/Entities/Account.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseProvider.Entities;
+
+internal class Account
+{
+    [Required]
+    [Key]
+    public int Sequence { get; set; }
+    [Required]
+    public string Number { get; set; }
+}
---
+++ DatabaseProvider/Entities/Representative.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseProvider.Entities;
+
+internal class Representative
+{
+    [Required]
+    [Key]
+    public int Sequence { get; set; }
+    [Required]
+    public Person Person { get; set; }
+}
